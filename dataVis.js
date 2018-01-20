215e1cec8d2279dd91ac9143cd8aecdca9b794f0
Lucas Grönborg <lucas94gronborg@gmail.com>
2018-01-20 17:34:38 +0100

    Success with parsing data from CSV

--- dataVis.js
+++ dataVis.js
@@ -21,30 +21,24 @@ svg.selectAll("text")
           .attr("x", function(d, i) {return (i * 60) + 12})
           .attr("y", function(d, i) {return 420 - (d * 10)});
 
-/*
 d3.csv(dataSheet, function(d) {
   return {
     alias : d.Alias,
-    major : d.What is your Major?,
-    personalInfo : d.Please, tell me about yourself. What interest you? Do you have any hobbies?,
-    skillViz : d.How would you rate your Information Visualization skills?,
-    skillStats : d.How would you rate your statistical skills?,
-    skillMath : d.How would you rate your mathematics skills?,
-    skillArt : d.How would you rate your drawing and artistic skills?,
-    skillComp : d.How would you rate your computer usage skills?,
-    skillProg : d.How would you rate your programming skills?,
-    skillGraph : d.How would you rate your computer graphics programming skills?,
-    skillHCI : d.How would you rate your human-computer interaction programming skills?,
-    skillUX : d.How would you rate your user experience evaluation skills?,
-    skillComm : d.How would you rate your communication skills?,
-    skillCollab : d.How would you rate your collaboration skills?,
-    skillRepo : d.How would you rate your code repository skills?,
+    major : d["What is your Major?"],
+    personalInfo : d["Please, tell me about yourself. What interest you? Do you have any hobbies?"],
+    skillViz : +d["How would you rate your Information Visualization skills?"],
+    skillStats : +d["How would you rate your statistical skills?"],
+    skillMath : +d["How would you rate your mathematics skills?"],
+    skillArt : +d["How would you rate your drawing and artistic skills?"],
+    skillComp : +d["How would you rate your computer usage skills?"],
+    skillProg : +d["How would you rate your programming skills?"],
+    skillGraph : +d["How would you rate your computer graphics programming skills?"],
+    skillHCI : +d["How would you rate your human-computer interaction programming skills?"],
+    skillUX : +d["How would you rate your user experience evaluation skills?"],
+    skillComm : +d["How would you rate your communication skills?"],
+    skillCollab : +d["How would you rate your collaboration skills?"],
+    skillRepo : +d["How would you rate your code repository skills?"]
   };
 }, function(data) {
-  console.log(data[0]);
-});
-*/
-
-d3.csv(dataSheet, function(data) {
   console.log(data[0]);
 });
