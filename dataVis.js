32a61e55fdb2d5158955373d14072417065ed2d5
Lucas Grönborg <lucas94gronborg@gmail.com>
2018-01-20 17:07:36 +0100

    Changed name on csv

--- dataVis.js
+++ dataVis.js
@@ -1,5 +1,5 @@
 var dataArray = [23, 13, 21, 14, 37, 15, 18, 34, 30];
-var dataSheet = "Anonymized Self-Introduction to IVIS18 (Responses) - Form Responses 1.csv";
+var dataSheet = "data.csv";
 var svg = d3.select("body").append("svg")
             .attr("height","100%")
             .attr("width","100%");
